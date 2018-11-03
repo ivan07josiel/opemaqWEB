79bc4aef51726f51d3050bbde1809bf5ca1ed4e2
Ivan Josiel <ivan07josiel@gmail.com>
2018-11-02 23:34:28 -0300

    Inicio da implementação da tela de cadastro de propriedades

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,27 @@
 <?php
 	session_start();
 	ob_start();
+	$btCadastro = filter_input(INPUT_POST, 'btCadastro' , FILTER_SANITIZE_STRING);
+  	if($btCadastro){
+		include_once 'conexao.php';
+		$dados = filter_input_array(INPUT_POST,FILTER_DEFAULT);
+		//var_dump($dados);
+		$dados['senha'] = password_hash($dados['senha'],PASSWORD_DEFAULT);
+
+		$inserirUsuarioBD = "INSERT INTO login (usuario,email,senha) VALUES(
+		'".$dados['usuario']."',
+		'".$dados['email']."',
+		'".$dados['senha']."'
+		)";
+		$resultadoUsuario = mysqli_query($conn,$inserirUsuarioBD);
+		if(mysqli_insert_id($conn)){
+			$_SESSION['msgcadastro'] = "Usuário cadastrado com sucesso";
+			header("Location: index.php");
+		}
+		else{
+			$_SESSION['msg'] = "Erro ao cadastrar o usuário";
+		}
+  	}
 ?>
 
 <!DOCTYPE html>
@@ -9,25 +30,25 @@
 	<meta charset="utf-8">
 	<title>Opemaq - Cadastro</title>
 </head>
-<body>
-	<h2>Cadastro</h2>
+<body class="text-center">
 	<?php
 		if(isset($_SESSION['msg'])){
 			echo $_SESSION['msg'];
 			unset($_SESSION['msg']);
 		}
 		/**
-		*Se a pagina for atualizada e
-		*a variavel $_SESSION tiver
-		*valor, ele reseta a variavel.
-		*/
-	?>
-	<form method="POST" action="">
-		<input type="text" name="ctNome" placeholder="Digite seu nome"><br><br>
-		<input type="text" name="ctEmail" placeholder="Digite seu email"><br><br>
-		<input type="text" name="ctUsuario" placeholder="Digite seu usuario"><br><br>
-		<input type="password" name="ctSenha" placeholder="Senha"><br>
-		<input type="submit" name="btCadastro" value="cadastrar">
+		 *Se a pagina for atualizada e
+		 *a variavel $_SESSION tiver
+		 *valor, ele reseta a variavel.
+		 */
+		?>
+	<form class="form-signin" method="POST" action="">
+		<h2 class="h3 mb-3 font-weight-normal">Cadastro</h2>
+		<input type="text" name="ctNome" placeholder="Digite seu nome" class="form-control"><br><br>
+		<input type="text" name="ctEmail" placeholder="Digite seu email" class="form-control"><br><br>
+		<input type="text" name="ctUsuario" placeholder="Digite seu usuario" class="form-control"><br><br>
+		<input type="password" name="ctSenha" placeholder="Senha" class="form-control"><br>
+		<input type="submit" name="btCadastro" value="cadastrar" class="btn btn-lg btn-primary btn-block">
 		<h4>Ja possui uma conta?</h4>
 		<a href="index.php">Entrar!</a>
 	</form>
